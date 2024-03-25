2cfeb92b23880cc8cd663c707377d5ca7124715e
lephanbaongoc <lpbn2003@gmail.com>
2024-03-25 23:41:21 +0700

    Cau3 tạo list Kỹ năng

--- Cau3_AppProfile/app/src/main/java/ntu_63130868/cau3_appprofile/MainActivity.java
+++ Cau3_AppProfile/app/src/main/java/ntu_63130868/cau3_appprofile/MainActivity.java
@@ -4,11 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> dsKynang;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        dsKynang = new ArrayList<String>();
+
+        dsKynang.add("Lập trình C/C++");
+        dsKynang.add("Lập trình C#");
+        dsKynang.add("Lập trình Java");
+        dsKynang.add("Lập trình Python");
+        dsKynang.add("Thông thạo bộ công cụ office");
+        dsKynang.add("Kỹ năng mềm tốt");
+        dsKynang.add("Kỹ năng làm việc nhóm");
+        dsKynang.add("Kỹ năng quản lý thời gian");
+
 }
