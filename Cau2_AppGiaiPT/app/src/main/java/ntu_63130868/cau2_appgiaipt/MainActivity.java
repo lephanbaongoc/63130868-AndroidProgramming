a1607ec574aaddb121a786aa83cd122c6c489089
lephanbaongoc <lpbn2003@gmail.com>
2024-03-25 22:41:58 +0700

    Cau2 thêm void TimX

--- Cau2_AppGiaiPT/app/src/main/java/ntu_63130868/cau2_appgiaipt/MainActivity.java
+++ Cau2_AppGiaiPT/app/src/main/java/ntu_63130868/cau2_appgiaipt/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +16,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void TimX(View view){
-        
+        //tìm tham chiếu
+        EditText edtSOA = findViewById(R.id.edtSoA);
+        EditText edtSOB = findViewById(R.id.edtSoB);
+        EditText edtSOC = findViewById(R.id.edtSoC);
+        EditText edtKQ = findViewById(R.id.edtKQ);
+        RadioButton rbtnBac1 = findViewById(R.id.rbtnB1);
+        RadioButton rbtnBac2 = findViewById(R.id.rbtnB2);
+
+        //lấy dữ liệu về
+        String strSOA = edtSOA.getText().toString();
+        String strSOB = edtSOB.getText().toString();
+        String strSOC = edtSOC.getText().toString();
+
+        //chuyển kiểu dữ liệu
+        double SOA =Integer.parseInt(strSOA);
+        double SOB =Integer.parseInt(strSOB);
+        double SOC =Integer.parseInt(strSOC);
+
+        //tính toán
+        if(rbtnBac1.isChecked() == true){
+
+        }
+        if(rbtnBac2.isChecked() == true){
+
+        }
     }
 }
