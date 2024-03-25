ac4ec5e718b775ebc6bd763681322e9efc3abba3
lephanbaongoc <lpbn2003@gmail.com>
2024-03-25 23:50:15 +0700

    Cau3 done

--- Cau3_AppProfile/app/src/main/java/ntu_63130868/cau3_appprofile/MainActivity.java
+++ Cau3_AppProfile/app/src/main/java/ntu_63130868/cau3_appprofile/MainActivity.java
@@ -3,8 +3,11 @@ package ntu_63130868.cau3_appprofile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -33,6 +36,16 @@ public class MainActivity extends AppCompatActivity {
                 android.R.layout.simple_list_item_1,dsKynang);
 
         ListView lvKyNang = findViewById(R.id.lvDanhSachKyNang);
-
+        lvKyNang.setAdapter(adapterTT);
+        lvKyNang.setOnItemClickListener(BoLangNgheVaXL);
     }
+
+    AdapterView.OnItemClickListener BoLangNgheVaXL = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            //Hiện lên màn hình thông báo nhanh về giá trị của phần tử vừa chọn
+            String StrTenKyNangChon = dsKynang.get(i);
+            Toast.makeText(MainActivity.this,"Bạn vừa chọn "+StrTenKyNangChon,Toast.LENGTH_LONG).show();
+        }
+    };
 }
