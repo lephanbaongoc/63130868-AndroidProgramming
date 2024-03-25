9fb71ef89b7b4c015349f0e3cec732452b72ea9c
lephanbaongoc <lpbn2003@gmail.com>
2024-03-25 20:42:15 +0700

    Cau2 start

---
+++ Cau2_AppGiaiPT/app/src/main/java/ntu_63130868/cau2_appgiaipt/MainActivity.java
@@ -0,0 +1,14 @@
+package ntu_63130868.cau2_appgiaipt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
